2319e9cffc36d2d86ad7104214916121bfa9083e
BearWulfx <torushojiro@gmail.com>
2021-10-26 17:03:12 +0300

    added ray getters and setters

--- src/Primitives/Ray.java
+++ src/Primitives/Ray.java
@@ -4,4 +4,23 @@ public class Ray {
     private Point3D head;
     private Vector direction;
 
+    public Ray(){
+        this.head = new Point3D();
+        this.direction = new Vector();
+    }
+    public Ray(double xPoint, double yPoint, double zPoint,
+               double xVector, double yVector, double zVector){
+        this.head  = new Point3D(xPoint, yPoint, zPoint);
+        this.direction = new Vector(xVector, yVector, zVector);
+    }
+    public Ray(Ray other){
+        this.head = new Point3D(other.getHead());
+        this.direction = new Vector(other.getDirection());
+    }
+    public Ray(Point3D head, Vector direction){
+        this.head = new Point3D(head);
+        this.direction = new Vector(direction);
+    }
+    public Vector getDirection() { return direction; }
+    public Point3D getHead() { return head; }
 }
