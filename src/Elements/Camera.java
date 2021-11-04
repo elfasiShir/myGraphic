0fec60ab6f21699d546cc86a24322c1d4d60097d
elfasishir <torushojiro@gmail.com>
2021-11-04 19:41:49 +0200

    added camera

---
+++ src/Elements/Camera.java
@@ -0,0 +1,20 @@
+package Elements;
+
+import Primitives.Point3D;
+import Primitives.Ray;
+import Primitives.Vector;
+
+public class Camera {
+    private Point3D P0;
+    private Vector vUp;
+    private Vector vLeft;
+    private Vector vTo;
+
+
+    public Ray constructRayThroughPixel(int nX, int nY, int j, int i,
+                                        double screenDistance, double screenWidth, double screenHeight) {
+
+
+
+    }
+}
